c78a299d3d4159f1f1336cfdab9e9b16db5877ae
alexandrafren <alexandra.fren@gmail.com>
2018-11-15 14:25:41 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :reviews
-  resources :games
-  resources :users
+  resources :games, only: [:new, :create, :show, :index]
+  resources :users, only: [:show, :new, :create, :delete]
+
+  get 'login', to: 'users#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
