62801e56c0926d24e47aa4a6c3a836ca4c085b4e
alexandrafren <alexandra.fren@gmail.com>
2018-11-15 16:54:48 -0800

    added game#show view content

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-console = Console.create(name: "PS4")
-game = Game.create(name: "God of War", release_year: 2018)
-game.consoles << console
-game.save
+c1 = Console.create(name: "PS4")
+c2 = Console.create(name:"Xbox One")
+c3 = Console.create(name:"Switch")
+g1 = Game.create(name: "God of War", release_year: 2018)
+g1.consoles << c1
+g2 = Game.create(name:"Red Dead Redepmtion 2", release_year: 2018)
+g2.consoles << c1
+g2.consoles << c2
+u1 = User.create(name: "firstuser", password_digest: "password")
+r1 = Review.create(rating: 5, content: "Game is most dope.", user_id: 1, game_id: 1)
