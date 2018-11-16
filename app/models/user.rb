5f5900a3d3f1122815d9619c58c3c2df01250261
alexandrafren <alexandra.fren@gmail.com>
2018-11-15 19:50:04 -0800

    save

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
 	has_many :games, through: :reviews
 	has_secure_password
 
+	def self.most_liked
+		self.joins(:reviews).where(reviews: {user_id: self.id}).order(rating: :asc).limit(3)
+	end
+
 end
