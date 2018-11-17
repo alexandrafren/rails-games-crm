40c02860b1e37dff137af3c40a6e78093ed30bd3
Alexandra Fren <alexandra.fren@gmail.com>
2018-11-17 23:20:57 +0000

    set up basic nested forms functionality in games for reviews

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,5 @@ class Game < ApplicationRecord
 	has_many :users, through: :reviews
 	has_many :console_games
 	has_many :consoles, through: :console_games
+	accepts_nested_attributes_for :reviews
 end
