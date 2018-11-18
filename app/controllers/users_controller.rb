a9d373b44a442b3b9818805f0fed9a64e30c6dbf
Alexandra Fren <alexandra.fren@gmail.com>
2018-11-18 01:00:44 +0000

    created sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,28 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def login
+		if logged_in?
+			redirect_to '/games'
+		else
+			render :'/users/login'
+		end
+	end
+
+	def create
+    user = User.new(user_params)
+    user.save
+    session[:user_id] = user.id
+    redirect_to '/games'
+	end
+
+private
+	def logged_in?
+		session[:user_id]
+	end
+
+	def user_params
+		params.require(:user).permit(:username, :password)
+	end
 end
