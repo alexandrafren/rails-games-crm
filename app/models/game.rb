1fc9502c1e208ff0eba6854943d65c66158ba979
Alexandra Fren <alexandra.fren@gmail.com>
2018-11-18 22:19:00 +0000

    custom validation messages

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,7 +4,8 @@ class Game < ApplicationRecord
 	has_many :console_games
 	has_many :consoles, through: :console_games
 	accepts_nested_attributes_for :reviews
-	validates :release_year, numericality: {greater_than: 1949}
-	validates :release_year, numericality: {less_than_or_equal_to: 2018}
+	validates :release_year, numericality: {greater_than: 1949, message: "release year must be 1950 or more recent"}
+	validates :release_year, numericality: {less_than_or_equal_to: 2018, message: "release year must be current year or less"}
 	validates :name, :release_year, presence: true
+	validates :name, uniqueness: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
 	belongs_to :game
 	belongs_to :user
-	validates :content, length: {in: 20..200}
-	validates :rating, numericality: {only_integer: true}
+	validates :content, length: {in: 20..200, message: "reviews must be between 20 and 200 charecters"}
+	validates :rating, numericality: {only_integer: true, message: "rating must be an integer"}
 	validates :rating, presence: true
 end
