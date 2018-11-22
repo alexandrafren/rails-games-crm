31cdd60334a1baebf298b72c4f43c30cdce39691
alexandrafren <alexandra.fren@gmail.com>
2018-11-21 16:55:23 -0800

    add validation back to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	has_many :reviews
 	has_many :games, through: :reviews
 	has_secure_password
-	validates :name, presence: true
+	validates :name, :password, presence: true
 	validates :name, uniqueness: true
 
 
